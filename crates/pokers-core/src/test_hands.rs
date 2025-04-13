a6d1f54c20ee57cc048b819d8c88e073a26016ee
Jacky <gokurohono@gmail.com>
2025-04-13 21:50:37 +1000

    test: adding more simple test cases for determining hand

--- crates/pokers-core/src/test_hands.rs
+++ crates/pokers-core/src/test_hands.rs
@@ -37,6 +37,70 @@ mod tests {
             let _res = determine_hand(cards).unwrap();
             assert_eq!(_res, Hand::RoyalFlush);
         }
- 
+    }
+    
+    #[test]
+    fn test_straight_flush() {
+        let suits = vec![
+            Suit::Diamond,
+            Suit::Club,
+            Suit::Heart,
+            Suit::Spade,
+        ];
+        for suit in suits.iter() {
+            let mut cards: Vec<Card> = Vec::new();
+            cards.push(Card {
+                suit: *suit,
+                number: CardNumber::Six,
+            });
+            cards.push(Card {
+                suit: *suit,
+                number: CardNumber::Five,
+            });
+            cards.push(Card {
+                suit: *suit,
+                number: CardNumber::Four,
+            });
+            cards.push(Card {
+                suit: *suit,
+                number: CardNumber::Three,
+            });
+            cards.push(Card {
+                suit: *suit,
+                number: CardNumber::Two,
+            });
+            // result
+            let _res = determine_hand(cards).unwrap();
+            assert_eq!(_res, Hand::StraightFlush);
+        }
+    }
+
+    #[test]
+    fn test_high_card() {
+
+        let mut cards: Vec<Card> = Vec::new();
+        cards.push(Card {
+            suit: Suit::Club,
+            number: CardNumber::Ace,
+        });
+        cards.push(Card {
+            suit: Suit::Spade,
+            number: CardNumber::Five,
+        });
+        cards.push(Card {
+            suit: Suit::Club,
+            number: CardNumber::Four,
+        });
+        cards.push(Card {
+            suit: Suit::Heart,
+            number: CardNumber::Three,
+        });
+        cards.push(Card {
+            suit: Suit::Diamond,
+            number: CardNumber::Two,
+        });
+        // result
+        let _res = determine_hand(cards).unwrap();
+        assert_eq!(_res, Hand::HighCard);
     }
 }
