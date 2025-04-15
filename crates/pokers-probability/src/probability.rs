9df9827701e38178655365b1766562a875e7296d
Jacky <gokurohono@gmail.com>
2025-04-15 21:57:09 +1000

    feat: calculating odd of straight flush

--- crates/pokers-probability/src/probability.rs
+++ crates/pokers-probability/src/probability.rs
@@ -1,5 +1,5 @@
 // use pokers_core::hand
-use pokers_core::cards::Card
+use pokers_core::cards::Card;
 
 pub fn calculate_better_hand_probability(user_hand: &[Card]) -> f64 {
 
@@ -21,5 +21,17 @@ fn probability_of_five_hand_cards() -> i32{
 fn probability_of_royal_flush() -> f64 {
     let num_suit_combinations = 4;
     let total_number_of_5_hand_cards = probability_of_five_hand_cards();
-    return num_suit_combinations / total_number_of_5_hand_cards;
+    return (num_suit_combinations as f64) / (total_number_of_5_hand_cards as f64);
+}
+
+// probability of royal flush with sliding window
+// {10..A}
+// {9..K}
+// {8..Q}
+// ...
+// {1..5}
+// straight flush is total number minus probability of royal flush.
+fn probability_of_straight_flush() -> f64 {
+    let total_num_straight_flush_including_royal = 10 * probability_of_royal_flush();
+    return total_num_straight_flush_including_royal - probability_of_royal_flush();
 }
